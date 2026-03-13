1946f84d5202ea7b0d4d70cc6f3a0cea4847da74
Gamezordd <amartyamishra8@gmail.com>
2026-03-13 21:38:55 +0530

    change twitter card to summary;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image" content={ogImage} />
         <meta property="og:image:width" content="512" />
         <meta property="og:image:height" content="512" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="GetOut" />
         <meta
           name="twitter:description"
